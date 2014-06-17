3e02b0f5987eeabb3ed303d9d0145ba33e7e7596
cyokodog <cyokodog@gmail.com>
2014-06-17 19:54:31 +0900

    add picasa zoom

--- picasa-zoom/jquery.picasa-zoom.js
+++ picasa-zoom/jquery.picasa-zoom.js
@@ -13,12 +13,13 @@
  *
  */
 ;(function($){
-	$.fn.picasaZoom = function(){
+	var plugin = $.fn.picasaZoom = function( option ){
+		var c = $.extend({}, plugin.defaults, option );
 		var target = this;
 		var link = target.wrap('<a href="javascript:void(0)" class="picasa-zoom"/>').parent();
 		link.on('click', function(){
 			var img = $(this).find('img').css('opacity', 0.5);
-			var size = ['/s144/', '/s800/'];
+			var size = [c.thumKey, c.pictKey];
 			var src = img.prop('src');
 			size = (src.search(size[0]) < 0) ? size.reverse() : size;
 			var src = img.prop('src').replace(size[0], size[1]);
@@ -28,5 +29,9 @@
 			}).prop('src',src);
 		});
 		return target;
+	}
+	plugin.defaults = {
+		thumKey : '/s144/',
+		pictKey : '/s800/',
 	}
 })(jQuery);
