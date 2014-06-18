a3b4cea4f8c845b798c28ee7e9f4be914768a141
cyokodog <cyokodog@gmail.com>
2014-06-18 09:48:34 +0900

    [v0.2] pacasa zoom アイコンを表示

--- picasa-zoom/jquery.picasa-zoom.js
+++ picasa-zoom/jquery.picasa-zoom.js
@@ -1,5 +1,5 @@
 /*
- * 	Picasa Zoom 0.1 - jQuery plugin
+ * 	Picasa Zoom 0.2 - jQuery plugin
  *	written by cyokodog
  *
  *	Copyright (c) 2014 cyokodog 
@@ -14,23 +14,29 @@
  */
 ;(function($){
 	var plugin = $.fn.picasaZoom = function( option ){
-		var c = $.extend({}, plugin.defaults, option );
-		var target = this;
-		var link = target.wrap('<a href="javascript:void(0)" class="picasa-zoom"/>').parent();
-		link.on('click', function(){
-			var img = $(this).find('img').css('opacity', 0.5);
-			var size = [c.thumKey, c.pictKey];
-			var src = img.prop('src');
-			size = (src.search(size[0]) < 0) ? size.reverse() : size;
-			var src = img.prop('src').replace(size[0], size[1]);
-			var dummy = $('<img/>').on('load', function(){
-				img.prop('src', src).css('opacity', 1).hide().fadeIn();
-				dummy.remove();
-			}).prop('src',src);
+		return this.each(function(){
+			var c = $.extend({}, plugin.defaults, option );
+			var target = $(this);
+			var link = target.wrap('<a href="javascript:void(0)" class="picasa-zoom"/>').parent();
+			var disp = target.css('display');
+			link.css({'display': (disp += (disp == 'inline' ? '-block' : ''))});
+			!c.useIcon || (c.icon = $('<span class="picasa-zoom-icon">+</span>').appendTo(link));
+			link.on('click', function(){
+				var img = $(this).find('img').css('opacity', 0.5);
+				var size = [c.thumKey, c.pictKey];
+				var src = img.prop('src');
+				size = (src.search(size[0]) < 0) ? size.reverse() : size;
+				src = img.prop('src').replace(size[0], size[1]);
+				var dummy = $('<img/>').on('load', function(){
+					img.prop('src', src).css('opacity', 1).hide().fadeIn();
+					dummy.remove();
+				}).prop('src',src);
+				!c.icon || c.icon.text(c.icon.text() == '+' ? '-' : '+');
+			});
 		});
-		return target;
 	}
 	plugin.defaults = {
+		useIcon : true,
 		thumKey : '/s144/',
 		pictKey : '/s800/',
 	}
